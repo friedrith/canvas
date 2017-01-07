f4091edb7cd9c49eddd20544f731c3ea7d163581
Thibault Friedrich <tf@elqui.fr>
2017-01-07 14:35:59 +0100

    manage different type of database

--- src/server/config/sequelize.js
+++ src/server/config/sequelize.js
@@ -8,18 +8,21 @@ var dataDirPath = path.join(__dirname, '../../../', config.data);
 
 fs.ensureDirSync(dataDirPath);
 
-var sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+var options = {
   host: 'localhost',
-  dialect: 'sqlite',
+  dialect: process.env.DB_TYPE,
 
   pool: {
     max: 5,
     min: 0,
     idle: 10000
   },
+};
 
-  // SQLite only
-  storage: path.join(dataDirPath, 'data.sqlite')
-});
+if (process.env.DB_TYPE == 'sqlite') {
+    options.storage = path.join(dataDirPath, 'data.sqlite');
+}
+
+var sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, options);
 
 module.exports = sequelize;
